06b76efef3f4a4aebb44c4bf97b6b1652eba21f7
Daniel Aparicio <daparicio52@gmail.com>
2017-06-19 18:54:36 -0400

    Cambio en base de datos

--- base de datos/database.sql
+++ base de datos/database.sql
@@ -1,11 +1,37 @@
-CREATE TABLE `cursos` (
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.6.31 - MySQL Community Server (GPL)
+-- Server OS:                    Win32
+-- HeidiSQL Version:             9.4.0.5125
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for CFC
+DROP DATABASE IF EXISTS `CFC`;
+CREATE DATABASE IF NOT EXISTS `CFC` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `CFC`;
+
+-- Dumping structure for table CFC.cursos
+DROP TABLE IF EXISTS `cursos`;
+CREATE TABLE IF NOT EXISTS `cursos` (
   `curso_id` int(11) NOT NULL AUTO_INCREMENT,
   `sigla` varchar(7) NOT NULL,
   `titulo` varchar(30) NOT NULL,
-  `resumen` varchar(500) NOT NULL,
+  `resumen` varchar(2048) NOT NULL,
   `fecha_inicio` date NOT NULL,
   `docente` varchar(50) NOT NULL,
   `imagename` varchar(50) NOT NULL,
   `imagecontent` longblob NOT NULL,
   PRIMARY KEY (`curso_id`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
