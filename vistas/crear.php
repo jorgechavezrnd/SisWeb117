0797f4d0322b989b89b7da21afd8bba10deef9ad
Daniel Aparicio <daparicio52@gmail.com>
2017-06-23 11:10:37 -0400

    Verificacion editar y crear 3

--- vistas/crear.php
+++ vistas/crear.php
@@ -2,7 +2,7 @@
 	$controlador = new ControladorCursos();
 	if (isset($_POST['enviar'])) {
     if($_FILES['image']['tmp_name']==null){
-      echo "Ingrese una imagen";
+      echo "<p style='font-size: 20pt;'>Ingrese una imagen</p>";
     }else{
       if(verificar($_POST['sigla'])){
         if(verificar($_POST['titulo'])){
@@ -13,18 +13,18 @@
               $image = file_get_contents($image);
               $image = base64_encode($image);
               $controlador->crear($_POST['sigla'], $_POST['titulo'], $_POST['resumen'], $_POST['fecha_inicio'], $_POST['docente'],$name,$image);
-              echo "Se ha registrado un nuevo curso";
+              echo "<p style='font-size: 20pt;'>Se ha registrado un nuevo curso</p>";
             }else{
-              echo "Caracteres invalidos en docente";
+              echo "<p style='font-size: 20pt;'>Caracteres invalidos en docente</p>";
             }
           }else{
-            echo "Caracteres invalidos en resumen";
+            echo "<p style='font-size: 20pt;'>Caracteres invalidos en resumen</p>";
           }
         }else{
-          echo "Caracteres invalidos en titulo";
+          echo "<p style='font-size: 20pt;'>Caracteres invalidos en titulo</p>";
         }
       }else{
-        echo "Caracteres invalidos en sigla";
+        echo "<p style='font-size: 20pt;'>Caracteres invalidos en sigla</p>";
       }
       	
     }
@@ -56,35 +56,35 @@
     <div class="form-group">
       <label for="inputEmail" class="col-lg-1 control-label">Titulo</label>
       <div class="col-lg-5">
-        <input type="text" maxlength="30" minlength="1" name="titulo" class="form-control" id="inputEmail" placeholder="Titulo" value="<?php if(isset($_POST['titulo'])){echo $_POST['titulo'];}?>" required="required">
+        <input type="text" maxlength="30" minlength="1" name="titulo" class="form-control" id="inputEmail" placeholder="Titulo" value="<?php if(isset($_POST['titulo']) && verificar($_POST['titulo'])){echo $_POST['titulo'];}?>" required="required">
       </div>
     </div>
 
     <div class="form-group">
       <label for="inputEmail" class="col-lg-1 control-label">Sigla</label>
       <div class="col-lg-5">
-        <input type="text" maxlength="8" name="sigla" class="form-control" id="inputEmail" placeholder="Sigla" value="<?php if(isset($_POST['sigla'])){echo $_POST['sigla'];}?>" required="required">
+        <input type="text" maxlength="8" name="sigla" class="form-control" id="inputEmail" placeholder="Sigla" value="<?php if(isset($_POST['sigla']) && verificar($_POST['sigla'])){echo $_POST['sigla'];}?>" required="required">
       </div>
     </div>
 
     <div class="form-group">
       <label for="inputdate" class="col-lg-1 control-label">Fecha inicio</label>
       <div class="col-lg-5">
-        <input type="date" name="fecha_inicio" class="form-control" id="inputPassword" placeholder="Fecha inicio" value="<?php if(isset($_POST['fecha_inicio'])){echo $_POST['fecha_inicio'];}?>" required="required">
+        <input type="date" name="fecha_inicio" class="form-control" id="inputPassword" placeholder="Fecha inicio" value="<?php if(isset($_POST['fecha_inicio']) && verificar($_POST['fecha_inicio'])){echo $_POST['fecha_inicio'];}?>" required="required">
       </div>
     </div>
 
     <div class="form-group">
       <label for="inputDocente" class="col-lg-1 control-label">Docente</label>
       <div class="col-lg-5">
-  		  <input type="text" minlength="1" name="docente" class="form-control" id="inputDocente" placeholder="Docente" value="<?php if(isset($_POST['docente'])){echo $_POST['docente'];}?>" required="required">
+  		  <input type="text" minlength="1" name="docente" class="form-control" id="inputDocente" placeholder="Docente" value="<?php if(isset($_POST['docente']) && verificar($_POST['docente'])){echo $_POST['docente'];}?>" required="required">
       </div>
     </div>
 
     <div class="form-group">
       <label for="textArea" class="col-lg-1 control-label">Resumen</label>
       <div class="col-lg-5">
-        <textarea class="form-control" minlength="1" maxlength="2048" name="resumen" rows="12" id="textArea" required="required"><?php if(isset($_POST['resumen'])){echo $_POST['resumen'];}?></textarea>
+        <textarea class="form-control" minlength="1" maxlength="2048" name="resumen" rows="12" id="textArea" required="required"><?php if(isset($_POST['resumen']) && verificar($_POST['resumen'])){echo $_POST['resumen'];}?></textarea>
         <span class="help-block">Descripción del curso.</span>
       </div>
     </div>
